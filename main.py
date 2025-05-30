f8d99c878ffb31df0e13993cff2a2ba727450c84
Lorquette <153440088+Lorquette@users.noreply.github.com>
2025-05-30 18:46:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.43"
 
 KEYWORDS = [
-    "Pokémon", "Pokemon", "Destined Rivals", "Prismatic Evolutions", "Journey Together", "Palafin"
+    "Pokémon", "Pokemon", "Destined Rivals", "Prismatic Evolutions", "Journey Together", "Black Bolt", "White Flare"
 ]
 
 def load_json(file_path):
