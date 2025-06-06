627104b7e5cd9416c5b00a0fa3208877d7255d72
Lorquette <153440088+Lorquette@users.noreply.github.com>
2025-06-06 13:32:30 +0200

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -275,7 +275,7 @@ def read_sites_from_sheet():
     sh = gc.open_by_key(SPREADSHEET_ID_S)
     worksheet = sh.worksheet("Sites")
 
-    data = worksheet.get("A1:H29")
+    data = worksheet.get("A1:I40")
 
     # Första raden är nycklar (kolumnrubriker) - första kolumn är 'key'
     keys = data[0]
